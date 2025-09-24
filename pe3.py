80b87a08cd0f11025988e7d3bba8633327ec5f9d
Brandon-L-Medina <101159765+Brandon-L-Medina@users.noreply.github.com>
2025-09-23 19:02:42 -0700

    Built program shell

---
+++ pe3.py
@@ -0,0 +1,17 @@
+def main():
+    print("Welcome to the Ceasar Cipher")
+    choice = input("Would you like to encode or decode a message? ").lower()
+
+    if choice == "encode":
+        encode()
+    elif choice == "decode":
+        decode()
+    else:
+        print("Invalid choice. Please choose 'encode' or 'decode'.")
+
+def encode():
+
+def decode():
+
+if __name__ == "__main__":
+    main()
